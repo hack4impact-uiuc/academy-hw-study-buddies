cdab7eddd259efd7cea2cf5317174b1b8052131f
Aaron Alexander <a.alexander72003@gmail.com>
2021-11-08 23:54:30 -0600

    Hover function works

--- client/src/components/SessionSummary.jsx
+++ client/src/components/SessionSummary.jsx
@@ -5,8 +5,12 @@ function SessionSummary({ session }) {
   return (
     <Card centered className="sessionCard">
       <Card.Content className="insideCard">
-        <Card.Header>{session.creator} is studying {session.class} at {session.location}</Card.Header>  
-        <a href="uiucrejects.com" className="btn">JOIN</a>
+        <Card.Header>
+          {session.creator} is studying {session.class} at {session.location}
+        </Card.Header>
+        <a href="uiucrejects.com" className="btn">
+          JOIN
+        </a>
       </Card.Content>
     </Card>
   );

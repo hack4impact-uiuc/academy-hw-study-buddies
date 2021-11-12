551b9908ca875f2a3bf18e7c97e8b832b6c0d947
Estrella Popoca <65576812+exrlla@users.noreply.github.com>
2021-11-11 22:09:00 -0600

    Add Session Summary Component  (#18)
    * First push with SessionSummary
    
    * Add CSS Styling
    
    * Add font files
    
    * Add button
    
    * Add fonts
    
    * Hover function works
    
    * All comments addressed
    
    * Addressed changes
    
    * Final PR commit
    
    * Delete DS Store
    
    * Delete DS Store
    
    * Deleted HomeHeader
    
    Co-authored-by: Aaron Alexander <a.alexander72003@gmail.com>
    Co-authored-by: Aaron Alexander <82335572+AaronAlexander-debug@users.noreply.github.com>

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,25 @@
 import React, { useEffect, useState } from 'react';
 
+import SessionSummary from '../components/SessionSummary';
+// import HomeHeader from '../components/HomeHeader';
 import { getSampleResponse } from '../utils/apiWrapper';
 
 import '../css/Home.scss';
 
 function Home() {
+  const sessions = [
+    {
+      creator: 'Aaron Alexander',
+      class: 'CS 124',
+      location: 'Grainger Engineering Library',
+    },
+    {
+      creator: 'Aaron Alexander',
+      class: 'CS 124',
+      location: 'Grainger Engineering Library',
+    },
+  ];
+  const creator = 'Name Last';
   const [text, setText] = useState([]);
 
   useEffect(() => {
@@ -17,13 +32,15 @@ function Home() {
 
     populateText();
   }, []);
-
   return (
     <>
+      <h2 className="homeHeader">Welcome, {creator}</h2>
       <h1>Studying Activity</h1>
+      <p></p>
+      {sessions.map((session, i) => (
+        <SessionSummary session={session} key={i} />
+      ))}
       <p>
-        Insert studying activity here
-        <br />
         {text.length > 0
           ? `You have successfully fetched ${text.length} documents!`
           : 'You did not run local API!'}

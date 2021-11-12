b2250d0fc69322ff746a42ef927185f506ba41f1
Eesha <eesha.barua@gmail.com>
2021-11-11 22:13:04 -0600

    Accept Changes

--- api/src/api/session.js
+++ api/src/api/session.js
@@ -1,41 +1,100 @@
 const express = require('express');
 const router = express.Router();
 const { errorWrap } = require('../middleware');
-const { createResponse } = require('../utils');
-
-// uncomment to use the schema
 const Session = require('../models/session');
 
 router.post(
-    '/',
-    errorWrap(async (req, res) => {
-        const newSession = await Session.create(req.body);
-        if (newSession) {
+  '/',
+  errorWrap(async (req, res) => {
+    const newSession = await Session.create(req.body);
+    if (newSession) {
       res.status(200).json({
         message: 'Successfully created new session',
         success: true,
         result: newSession,
       });
     }
-})
-  );
+  }),
+);
 
 router.get(
-    '/',
-    errorWrap(async (req, res) => {
-      const session = await Session.findOne();
-  
-      // Template for formulating a successful API response
-      if (session) {
-        const statusCode = 200;
-        const responseBody = createResponse(
-          statusCode,
-          'Successfully returned session text',
-          session.text,
-        );
-        res.status(statusCode).json(responseBody);
-      }
-    }),
-  );
+  '/',
+  errorWrap(async (req, res) => {
+    const sessions = await Session.find({});
+    res.status(200).json({
+      message: `Successfully retrieved all sessions.`,
+      success: true,
+      result: sessions,
+    });
+    return;
+  }),
+);
+
+router.get(
+  '/:sessionId',
+  errorWrap(async (req, res) => {
+    const session = await Session.findById(req.params.sessionId);
+    if (!session) {
+      res.status(404).json({
+        success: false,
+        message: 'Session not found with id',
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        result: session,
+        message: 'Successfully retrieved session',
+      });
+    }
+  }),
+);
+
+router.put(
+  '/:sessionId',
+  errorWrap(async (req, res) => {
+    const updatedSession = await Session.findByIdAndUpdate(
+      req.params.sessionId,
+      req.body,
+      { new: true, runValidators: true },
+    );
+
+    if (!updatedSession) {
+      res.status(404).json({
+        success: false,
+        message: 'Session not found with id',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'Successfully updated session',
+      result: updatedSession,
+    });
+  }),
+);
+
+router.delete(
+  '/:sessionId',
+  errorWrap(async (req, res) => {
+    const deletedSession = await Session.findByIdAndDelete(
+      req.params.sessionId,
+    );
+
+    if (!deletedSession) {
+      res.status(404).json({
+        success: false,
+        message: 'Session not found with id',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'Session successfully deleted',
+      result: deletedSession,
+    });
+  }),
+);
 
 module.exports = router;
--- api/src/models/session.js
+++ api/src/models/session.js
@@ -5,11 +5,18 @@ const Session = new mongoose.Schema({
   class: { type: String, default: null, required: true },
   location: { type: String, default: null, required: true },
   attendees: { type: [String], default: null },
+<<<<<<< HEAD
   groupSize: { type: Number, default: 0 },
   notes: { type: String, default: null },
   active: { type: Boolean, default: false, required: true },
   startTime: { type: Number, default: Date.now() / 1000, required: true },
   timeout: { type: Number, default: 43200 }, // length of timeout in milliseconds, 12 hours
+=======
+  notes: { type: String, default: null },
+  active: { type: Boolean, default: false, required: true },
+  startTime: { type: Number, default: Date.now() / 1000, required: true },
+  timeout: { type: Number, default: 43200 }, // length of timeout in seconds, 12 hours
+>>>>>>> d239ffac97e522b2ef4519a55e3695e55395b480
 });
 
 module.exports = mongoose.model('Session', Session);

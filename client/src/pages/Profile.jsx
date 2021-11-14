74f4654252fe33407465c5afb7e9d3939db524ae
Jessica Singh <jessicasingh@outlook.com>
2021-11-14 14:58:21 -0600

    Add userId on top and Add Class button

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import '../css/Profile.scss';
+
 function Profile() {
-  return (
+  const userId = "618a03da68cc5220e45822c5";
+    return (
     <>
+    
       <h1>Profile Page</h1>
+      <h2>{userId}</h2>
+      <button className="add-class-button">Add class</button>
     </>
   );
 }

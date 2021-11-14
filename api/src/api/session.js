e6d5cc2170d235ebb17e400081d2f7e9e3d454aa
saxytony <anthony.cruzmacedo@protonmail.com>
2021-11-14 17:46:07 -0600

    Create end route in api/srs/api/session.js for retrieving sessions a user is a participant of

---
+++ api/src/api/session.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const router = express.Router();
+const { errorWrap } = require('../middleware');
+const Session = require('../models/session');
+
+router.post(
+  '/',
+  errorWrap(async (req, res) => {
+    const newSession = await Session.create(req.body);
+    if (newSession) {
+      res.status(200).json({
+        message: 'Successfully created new session',
+        success: true,
+        result: newSession,
+      });
+    }
+  }),
+);
+
+router.get(
+  '/',
+  errorWrap(async (req, res) => {
+    const sessions = await Session.find({});
+    res.status(200).json({
+      message: `Successfully retrieved all sessions.`,
+      success: true,
+      result: sessions,
+    });
+    return;
+  }),
+);
+
+router.get(
+  '/:sessionId',
+  errorWrap(async (req, res) => {
+    const session = await Session.findById(req.params.sessionId);
+    if (!session) {
+      res.status(404).json({
+        success: false,
+        message: 'Session not found with id',
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        result: session,
+        message: 'Successfully retrieved session',
+      });
+    }
+  }),
+);
+
+router.get(
+  '/attending/:userId',
+  errorWrap(async (req, res) => {
+    const attendingSessions = await Session.find({
+      userId: {
+        $in: [Session.attendees],
+      },
+    });
+
+    if (!attendingSessions) {
+      res.status(404).json({
+        success: false,
+        message: 'Could not retrieve sessions attended by user',
+      });
+      return;
+    }
+    res.status(200).json({
+      success: true,
+      message: 'Successfully retrieved sessions attended by user',
+      result: attendingSessions,
+    });
+    return;
+  }),
+);
+
+router.put(
+  '/:sessionId',
+  errorWrap(async (req, res) => {
+    const updatedSession = await Session.findByIdAndUpdate(
+      req.params.sessionId,
+      req.body,
+      { new: true, runValidators: true },
+    );
+
+    if (!updatedSession) {
+      res.status(404).json({
+        success: false,
+        message: 'Session not found with id',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'Successfully updated session',
+      result: updatedSession,
+    });
+  }),
+);
+
+router.delete(
+  '/:sessionId',
+  errorWrap(async (req, res) => {
+    const deletedSession = await Session.findByIdAndDelete(
+      req.params.sessionId,
+    );
+
+    if (!deletedSession) {
+      res.status(404).json({
+        success: false,
+        message: 'Session not found with id',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'Session successfully deleted',
+      result: deletedSession,
+    });
+  }),
+);
+
+module.exports = router;

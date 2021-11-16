874dd5458c693e2d88fefb04022f2b041c060bc8
Jessica Singh <jessicasingh@outlook.com>
2021-11-15 23:01:36 -0600

    Show dummy sessions and add 'add class' button'

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 
+import 'semantic-ui-css/semantic.min.css';
 import '../css/Profile.scss';
+import SessionSummary from '../components/SessionSummary';
+import ClassForm from '../components/ClassForm';
 
 function Profile() {
   const userId = "618a03da68cc5220e45822c5";
+  const sessions = [
+    {
+      creator: 'Aaron Alexander',
+      class: 'CS 124',
+      location: 'Grainger Engineering Library',
+    },
+    {
+      creator: 'Aaron Alexander',
+      class: 'CS 124',
+      location: 'Grainger Engineering Library',
+    },
+  ];
     return (
     <>
-    
       <h1>Profile Page</h1>
       <h2>{userId}</h2>
-      <button className="add-class-button">Add class</button>
-      <>
+      <h1>My Sessions</h1>
+      <p></p>
+      {sessions.map((session, i) => (
+        <SessionSummary session={session} key={i} />
+      ))}
+      <ClassForm
+              button={<button className="small ui button" id="add-class-btn">Add class</button>}
+      /> 
+      <button className="small ui button" id="add-class-btn">Add class</button>
     </>
   );
 }

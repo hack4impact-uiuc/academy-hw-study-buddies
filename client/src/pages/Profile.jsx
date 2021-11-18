55caa7cfee7b8835aed9b6de99fb9f3a80070c49
eeshabarua <61480751+eeshabarua@users.noreply.github.com>
2021-11-17 18:28:38 -0600

    Create Class Card Component (#19)
    * create session schema
    
    * Write post endpoints
    
    * Add Changes to App.js
    
    * Start Issue #14
    
    * Accept Changes
    
    * Add Class Card Component
    
    * Create Card with Delete Issue #14
    
    * Add Function in API Wrapper Issue #14
    
    * Run Yarn Format
    
    * Add Flexbox CSS
    
    * Update from PR Feedback
    
    * Create Class Card Component
    
    * Adjust Card Size
    
    * Run yarn format
    
    Co-authored-by: Grace Zhang <gracewzhang01@gmail.com>
    Co-authored-by: dyang5200 <danielleyang5200@gmail.com>

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import ClassCard from '../components/ClassCard';
+
 function Profile() {
   return (
     <>
       <h1>Profile Page</h1>
+      <ClassCard classCardText={'CS125'} />
     </>
   );
 }

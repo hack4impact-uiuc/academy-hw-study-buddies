44c146279eb05cd201e5924a841dadb1a950fc28
saxytony <anthony.cruzmacedo@protonmail.com>
2021-11-24 17:08:19 -0600

    Run yarn format

--- client/src/components/ClassPageCard.jsx
+++ client/src/components/ClassPageCard.jsx
@@ -4,21 +4,21 @@ import { Card } from 'semantic-ui-react';
 import '../css/ClassPageCard.scss';
 
 function ClassPageCard(props) {
-    const { course, classMembers } = props;
-    return (
+  const { course, classMembers } = props;
+  return (
     <>
-    <Card className="class-cards">
+      <Card className="class-cards">
         <Card.Content>
-            <Card.Header>{course}</Card.Header>
-            <Card.Description>
-                {classMembers.map((name) => (
-                    <p key={name}>{name}</p>
-                ))}
-            </Card.Description>
+          <Card.Header>{course}</Card.Header>
+          <Card.Description>
+            {classMembers.map((name) => (
+              <p key={name}>{name}</p>
+            ))}
+          </Card.Description>
         </Card.Content>
-    </Card>           
+      </Card>
     </>
-    );
+  );
 }
 
 export default ClassPageCard;

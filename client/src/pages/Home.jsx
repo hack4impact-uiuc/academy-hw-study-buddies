7547afd63988b8296f30fcc58fcf21bba412a971
dyang5200 <danielleyang5200@gmail.com>
2021-11-30 14:09:16 -0600

    Add images and message

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,8 @@ import { Button } from 'semantic-ui-react';
 
 import SessionSummary from '../components/SessionSummary';
 import SessionForm from '../components/SessionForm';
-
+import HomeImgSitting from '../utils/images/homeimg-sitting.png';
+import HomeImgLaying from '../utils/images/homeimg-laying.png';
 import '../css/Home.scss';
 
 function Home({ user }) {
@@ -22,13 +23,40 @@ function Home({ user }) {
 
   return (
     <>
-      <h1>
-        Studying activity for {user.firstName} {user.lastName}{' '}
+      <h1 className="welcome-msg">
+        Welcome Back, {user.firstName} {user.lastName}
       </h1>
-      {sessions.map((session, i) => (
-        <SessionSummary session={session} key={i} />
-      ))}
-      <SessionForm button={<Button type="default">+</Button>} id={user._id} />
+
+      <div className="studying-activity-container">
+        <h1>STUDYING ACTIVITY</h1>
+        {sessions.map((session, i) => (
+          <SessionSummary session={session} key={i} />
+        ))}
+        <SessionForm
+          button={
+            <Button className="add-session-btn" type="default">
+              +
+            </Button>
+          }
+          id={user._id}
+        />
+      </div>
+
+      <img
+        className="homeimg-sitting"
+        src={HomeImgSitting}
+        alt="HomeImgSitting"
+      ></img>
+      <img
+        className="homeimg-laying"
+        src={HomeImgLaying}
+        alt="HomeImgLaying"
+      ></img>
+      <div className="academy-msg">
+        <b>
+          Made with love from the Fa2021 Academy Team <br /> Hack4Impact 2021
+        </b>
+      </div>
     </>
   );
 }
